10d411984cc431bb53c330acf1c61ea630488647
Aaron Goh <aarongjy97@gmail.com>
2021-07-14 00:07:31 +0800

    auth login/register for patients done

--- auth/lambda/src/app.ts
+++ auth/lambda/src/app.ts
@@ -1,5 +1,6 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda"
-import { PatientModel } from './schema/Patient';
+import { APIGatewayProxyResult } from "aws-lambda";
+import { PatientModel, Patient } from "./schema/Patient";
+import * as bcrypt from "bcryptjs";
 
 export const lambdaHandler = async (
   event: any
@@ -8,34 +9,69 @@ export const lambdaHandler = async (
     const method = event.requestContext.http.method;
 
     switch (method) {
-      case 'POST':
+      case "POST":
         const registerData = JSON.parse(event.body);
-        break;
-      case 'GET':
+        return await register(registerData);
+      case "GET":
         const loginData = JSON.parse(event.body);
         return await login(loginData);
-        break;
-      case 'PUT':
-        return responseBuilder(500, 'Not built')
-      case 'DELETE':
-        return responseBuilder(500, 'Not built')
       default:
-        return responseBuilder(500, 'Unknown Error');
+        return responseBuilder(500, "Unknown Error");
     }
   } catch (error) {
-    return responseBuilder(500, JSON.stringify(error));
+    return responseBuilder(500, error.msg);
+  }
+};
+
+async function login(data: any): Promise<APIGatewayProxyResult> {
+  const { email, password } = data;
+  const query = await PatientModel.scan("email").eq(email).limit(1).exec();
+
+  if (query.length === 1) {
+    const patient = query[0] as Patient;
+    const passwordCheck = await bcrypt.compare(password, patient.password);
+
+    if (passwordCheck) {
+      patient.password = "";
+      return responseBuilder(200, JSON.stringify(patient));
+    } else {
+      return responseBuilder(401, "Invalid credentials");
+    }
+  } else {
+    return responseBuilder(401, "User not found");
   }
 }
 
-async function login(data: any) : Promise<APIGatewayProxyResult> {
-  const { email, password } = data;
-  const patient = await PatientModel.scan("email").eq(email).exec();
-  return responseBuilder(200, JSON.stringify(patient));
+async function register(data: any): Promise<APIGatewayProxyResult> {
+  const fields = data as Patient;
+
+  const query = await PatientModel.scan("email")
+    .eq(fields.email)
+    .limit(1)
+    .exec();
+
+  if (query.length != 0) {
+    return responseBuilder(422, "User already exists");
+  }
+
+  const salt = await bcrypt.genSalt();
+  const hash = await bcrypt.hash(fields.password, salt);
+  fields.password = hash;
+  const patient = new PatientModel(fields);
+  await patient.save();
+
+  return responseBuilder(
+    202,
+    `Patient ${fields.email} successfully registered`
+  );
 }
 
-function responseBuilder(statusCode: number, msg: string) : APIGatewayProxyResult {
+function responseBuilder(
+  statusCode: number,
+  msg: string
+): APIGatewayProxyResult {
   return {
     statusCode: statusCode,
-    body: msg
+    body: msg,
   };
 }
--- auth/lambda/src/schema/Patient.ts
+++ auth/lambda/src/schema/Patient.ts
@@ -2,17 +2,84 @@ import { model, Schema } from "dynamoose";
 import { Document } from "dynamoose/dist/Document";
 
 export interface Patient extends Document {
-  name: string;
-  password: string;
-  email: string;
+  name: string,
+  password: string,
+  email: string,
+  phone: string,
+  dob: Date,
+  address: string,
+  postalCode: Number,
+  allergies?: Allergy[],
+  healthConditions?: HealthCondition[],
+  babies?: Baby[]
 }
 
 const PatientSchema = new Schema({
   name: String,
   password: String,
-  email: String
+  email: String,
+  phone: String,
+  dob: Date,
+  address: String,
+  postalCode: Number,
 });
+
+export interface Allergy extends Document {
+  allergyId: string,
+  name: string,
+  description: string,
+  patientId: string
+}
+
+const AllergySchema = new Schema({
+  allergyId: String,
+  name: String,
+  description: String,
+  patientId: String
+})
+
+export interface HealthCondition extends Document {
+  healthConditionId: string,
+  name: string,
+  description: string,
+  patientId: string
+}
+
+const HealthConditionSchema = new Schema({
+  healthConditionId: String,
+  name: String,
+  description: String,
+  patientId: String
+})
+
+export interface Baby extends Document {
+  babyId: string,
+  patientId: string,
+  dueDate: Date,
+  babyName: string,
+  isBorn: boolean
+}
+
+const BabySchema = new Schema({
+  babyId: String,
+  patientId: String,
+  dueDate: Date,
+  babyName: String,
+  isBorn: Boolean
+})
 
 export const PatientModel = model<Patient>("patient", PatientSchema, {
   create: false,
 });
+
+export const AllergyModel = model<Allergy>("allergy", AllergySchema, {
+  create: false,
+})
+
+export const HealthConditionModel = model<HealthCondition>("health-condition", HealthConditionSchema, {
+  create: false,
+})
+
+export const BabyModel = model<Baby>("babies", BabySchema, {
+  create: false,
+})
