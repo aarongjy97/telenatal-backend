925d1960f1234293ce46fd804d3bc7734eca2273
Ashleigh <ashleighaathar@gmail.com>
2021-07-17 22:48:15 +0800

    add teleconference apis

---
+++ Teleconference/lambda/src/app.js
@@ -0,0 +1,111 @@
+import * as Chime from "aws-sdk/clients/chime";
+import { v4 as uuidv4 } from "uuid";
+import { APIGatewayProxyResult } from "aws-lambda";
+import * as bcrypt from "bcryptjs";
+import * as AWS from "aws-sdk";
+
+const acccessKeyId = ""; // add credentials here
+const secretAccessKey = ""; // add credentials here
+AWS.config.credentials = new AWS.Credentials(acccessKeyId, secretAccessKey);
+
+const region = "us-east-1";
+const chime = new Chime({ region: region });
+chime.endpoint = new AWS.Endpoint(
+  "https://service.chime.aws.amazon.com/console"
+);
+
+export const lambdaHandler = async (event) => {
+  try {
+    const method = event.requestContext.http.method;
+    switch (method) {
+      case "POST":
+        return await createChimeMeeting(event.queryStringParameters);
+      case "DELETE":
+        return await deleteChimeMeeting(event.queryStringParameters);
+      case "PUT":
+        return await joinChimeMeeting(event.queryStringParameters);
+      default:
+        return responseBuilder(500, "Unknown Error");
+    }
+  } catch (error) {
+    console.log(error);
+    return responseBuilder(500, error.message);
+  }
+};
+
+function responseBuilder(statusCode, msg) {
+  return {
+    statusCode: statusCode,
+    body: msg,
+  };
+}
+
+async function createChimeMeeting(title, name, region) {
+  var meeting = await chime.createMeeting({
+    ClientRequestToken: uuidv4(),
+  });
+
+  const joinInfo = {
+    JoinInfo: {
+      Title: title,
+      Meeting: meeting,
+    },
+  };
+
+  return responseBuilder(200, JSON.stringify(meeting));
+}
+
+async function joinChimeMeeting(query) {
+  var meeting;
+  var meetingId;
+  var title = query.title;
+
+  // initialise meeting
+  if (!query.meetingId) {
+    //new meeting
+    meetingId = uuidv4();
+    meeting = await chime
+      .createMeeting({
+        ClientRequestToken: meetingId,
+        MediaRegion: region,
+        ExternalMeetingId: meetingId,
+      })
+      .promise();
+  } else {
+    //join to existing meeting
+    meetingId = query.meetingId;
+    meeting = await chime
+      .getMeeting({
+        MeetingId: meetingId,
+      })
+      .promise();
+  }
+
+  // add attendees
+  const attendee = await chime
+    .createAttendee({
+      //ID of the meeting
+      MeetingId: meeting.Meeting.MeetingId,
+      //User ID that we want to associate to
+      ExternalUserId: uuidv4(),
+    })
+    .promise();
+
+  const joinInfo = {
+    JoinInfo: {
+      Title: title,
+      Meeting: meeting,
+      Attendee: attendee,
+    },
+  };
+
+  return responseBuilder(200, JSON.stringify(joinInfo));
+}
+
+// telecon/delete/meeting?title={title} [DELETE]
+async function deleteChimeMeeting(query) {
+  await chime.deleteMeeting({
+    MeetingId: meetingCache[title].Meeting.MeetingId,
+  });
+  return responseBuilder(200, "successfully deleted chime meeting");
+}
